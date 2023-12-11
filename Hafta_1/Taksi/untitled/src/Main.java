27fc56651c642b283321391f656d1822f3d5e182
jericho909 <omerahmetyucel@gmail.com>
2023-12-11 12:40:00 +0300

    added fourth hw

---
+++ Hafta_1/Taksi/untitled/src/Main.java
@@ -0,0 +1,30 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int mesafe;
+        double min = 10.0;
+        double ucret;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Lütfen gidilen mesafeyi KM bazında giriniz:");
+        mesafe = input.nextInt();
+
+        ucret = mesafe * 2.20;
+        ucret += min;
+
+
+        if (ucret < 20.0) {
+            ucret = 20.0;
+        }
+
+        //floating point error çözümüne baktım ama işi baya uzatıyordu. o yüzden burada entegre etmedim.
+
+
+        System.out.println("Taksi ücreti: " + ucret + " TL");
+
+    }
+}
