7e1563815abd64877e54648bde236ff212bf7c1d
jericho909 <omerahmetyucel@gmail.com>
2023-12-12 14:16:11 +0300

    added comments

---
+++ Hafta_1/Etkinlik_Onerisi/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       int sicaklik = 0, c;
+       boolean inputIsValid = false;
+       
+       Scanner input = new Scanner(System.in);
+       System.out.println("Lütfen hava sıcaklığını Celcius cinsinden giriniz: ");
+       while(!inputIsValid) {
+           if (input.hasNextInt()){
+               sicaklik = input.nextInt();
+               inputIsValid = true;
+               break;
+           } else {
+               String invalidInput = input.next();
+               System.out.println("Hatalı veri girişi yaptınız. Lütfen tekrardan deneyin.");
+           }
+       }
+       System.out.println("Hava sıcaklığı: " + sicaklik + " Celcius.");
+
+        if (sicaklik < 5) {
+            System.out.println("Sıcaklık 5'den düşük, 'Kayak' yapmayı öneririm.");
+        } else if (sicaklik >= 5 && sicaklik <= 15) {
+            System.out.println("Sıcaklık 5 ile 15 arasında, 'Sinema' etkinliğini öneririm.");
+        } else if (sicaklik > 15 && sicaklik <= 25) {
+            System.out.println("Sıcaklık 15 ile 25 arasında, 'Piknik' etkinliğini öneririm.");
+        } else {
+            System.out.println("Sıcaklık 25'ten büyük, 'Yüzme' etkinliğini öneririm.");
+        }
+    }
+}
