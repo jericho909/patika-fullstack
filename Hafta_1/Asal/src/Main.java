81796e471eea1a778580be5034a3e1d344dc8c89
jericho909 <omerahmetyucel@gmail.com>
2023-12-13 15:11:16 +0300

    initial commit

--- Hafta_1/Asal/src/Main.java
+++ Hafta_1/Asal/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Main {
+    //asal sayı bulma işlmei için fonksiyon tanımlandı
     static boolean isPrime(int num) {
         if(num<=1) {
             return false;
@@ -12,17 +13,20 @@ public class Main {
         return true;
     }
     public static void main(String[] args) {
+        //liste ve değişken oluşturuldu
         int x;
+        ArrayList<Integer> list = new ArrayList<Integer>();
         Scanner input = new Scanner(System.in);
         System.out.println("Lütfen sayıyı giriniz: ");
         x = input.nextInt();
-        ArrayList<Integer> list = new ArrayList<Integer>();
 
+        //döngü kuruldu bulunan asal sayılar listeye eklendi
         for(int i=2;i<=x;i++)
         {
             if(isPrime(i))
                 list.add(i);
         }
+        //liste yazdırıldı
         System.out.println("Asal sayılar: " + list);
     }
 }
