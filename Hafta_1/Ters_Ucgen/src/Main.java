96ce53c29b6405cf7c6994a29b21b8449780c601
jericho909 <omerahmetyucel@gmail.com>
2023-12-14 00:28:00 +0300

    initial commit

---
+++ Hafta_1/Ters_Ucgen/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i= 0; i<= n-1; i++)
+        {
+            for (int j=0; j<=i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=0; k<=n-1-i; k++)
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
