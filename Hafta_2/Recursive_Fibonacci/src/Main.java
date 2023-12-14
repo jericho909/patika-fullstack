f2c10fb1d4bb06781de74f03f4fdbe343a801165
jericho909 <omerahmetyucel@gmail.com>
2023-12-14 13:53:25 +0300

    edits

--- Hafta_2/Recursive_Fibonacci/src/Main.java
+++ Hafta_2/Recursive_Fibonacci/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class Main {
+    static int fib(int x){
+        if (x == 1 || x == 2){
+        return 1;}
+        return fib(x-1) + fib(x-2);
+    }
     public static void main(String[] args) {
-        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Kaç elemanlı Fibonacci serisi istiyorsunuz?");
+        int n = inp.nextInt();
+        for (int i = 1; i < n; i++){
+            list.add(fib(i));
+        }
+        System.out.println(list);
     }
 }
