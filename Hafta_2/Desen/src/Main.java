1042e006f61dd66d5a75eb39d56de7a7918b1652
jericho909 <omerahmetyucel@gmail.com>
2023-12-17 17:43:51 +0300

    Initial commit

--- Hafta_2/Desen/src/Main.java
+++ Hafta_2/Desen/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.*;
 public class Main {
+    public static void desen(int a){
+
+        if (a <= 0) {
+            System.out.print(a + " ");
+            return;
+        }
+
+        System.out.print(a + " ");
+        desen(a - 5);
+        System.out.print(a + " ");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen sayıyı girin: ");
+        int a = input.nextInt();
+        desen(a);
     }
 }
