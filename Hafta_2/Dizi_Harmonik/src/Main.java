38b103a2a64cc71fbc330ace684fc8ac8c6dfb12
jericho909 <omerahmetyucel@gmail.com>
2023-12-19 18:19:49 +0300

    switched to method

--- Hafta_2/Dizi_Harmonik/src/Main.java
+++ Hafta_2/Dizi_Harmonik/src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
-        double harmonicMean = 0;
-
-        for (int i: numbers) {
-            harmonicMean += 1.0 / i;
-        }
-        System.out.println(numbers.length / harmonicMean);
+        harmonicMean(numbers);
     }
 
+    public static void harmonicMean(int[] arr){
+        double harmonicMean = 0;
+
+        for (int i: arr) {
+            harmonicMean += 1.0 / i;
+        }
+        System.out.println("Dizinin harmonik ortalaması: " + arr.length / harmonicMean);
+    }
 
 }
