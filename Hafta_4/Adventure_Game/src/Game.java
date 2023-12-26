f7e486b32aab3fb2a4eea9d4128dea81791e8e0e
jericho909 <omerahmetyucel@gmail.com>
2023-12-26 17:07:05 +0300

    edits

--- Hafta_4/Adventure_Game/src/Game.java
+++ Hafta_4/Adventure_Game/src/Game.java
@@ -33,21 +33,21 @@ public class Game {
     public void gameStartMsg(int classChoice, Player player){
         switch (classChoice){
             case 1:
-                System.out.println("You are a Samurai. Valiant in battle, chained by your honor, " +
+                System.out.println("You, " + player.getPlayerName() + ", are a Samurai. Valiant in battle, chained by your honor, " +
                         "you are fast and deadly.");
                 System.out.println("You have " + player.getGameClass().getHealthPoint() +
                         " HP. Your attack damages opponents for " + player.getGameClass().getDmg() +
                         " HP. You have " + player.getGameClass().getMoney() + " coins in your pocket.");
                 break;
             case 2:
-                System.out.println("You are an Archer. Quick as a fox and deadly with a bow, " +
+                System.out.println("You, " + player.getPlayerName() + ", are an Archer. Quick as a fox and deadly with a bow, " +
                         "you wait for the perfect moment to strike.");
                 System.out.println("You have " + player.getGameClass().getHealthPoint() +
                         " HP. Your attack damages opponents for " + player.getGameClass().getDmg() +
                         " HP. You have " + player.getGameClass().getMoney() + " coins in your pocket.");
                 break;
             case 3:
-                System.out.println("You are a Knight. Heavy as a mountain and furious as the sun, " +
+                System.out.println("You, " + player.getPlayerName() + ", are a Knight. Heavy as a mountain and furious as the sun, " +
                         "your enemies flee just from your righteous gaze. ");
                 System.out.println("You have " + player.getGameClass().getHealthPoint() +
                         " HP. Your attack damages opponents for " + player.getGameClass().getDmg() +
